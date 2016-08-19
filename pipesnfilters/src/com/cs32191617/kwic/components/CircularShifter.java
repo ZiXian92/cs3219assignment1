1e4c79bea9baeb8cf640fd868aea456540832ef1
Joan Tan <joan.tan.xy@gmail.com>
2016-08-19 22:26:23 +0800

    Added circular shift component

--- pipesnfilters/src/com/cs32191617/kwic/components/CircularShifter.java
+++ pipesnfilters/src/com/cs32191617/kwic/components/CircularShifter.java
@@ -1,8 +1,7 @@
 package com.cs32191617.kwic.components;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by zixian on 8/18/16.
  */
@@ -14,6 +13,18 @@ public class CircularShifter {
      * @return A list of all generated indexes for the given input
      */
     public List<String> generateIndexes(String str){
-        return Arrays.asList(str);
+        String[] wordList = str.split("\\s");
+        ArrayList<String> indexList= new ArrayList<String>();
+        String currentShift;
+        for (int i=0; i<wordList.length; i++) {
+            currentShift = "";
+            for(int j=i; j<(wordList.length + i ); j++){
+                currentShift += wordList[j % wordList.length];
+                if(j < (wordList.length + i - 1))
+                    currentShift += " ";
+            }
+            indexList.add(currentShift);
+        }
+        return indexList;
     }
 }
