7d37bd2ebea240400f151b41dc7e6e35e955fe3f
zixian92 <zixian1992@hotmail.com>
2016-08-19 13:06:55 +0800

    Adds junit test for circular shifter

--- pipesnfilters/src/com/cs32191617/kwic/components/CircularShifter.java
+++ pipesnfilters/src/com/cs32191617/kwic/components/CircularShifter.java
@@ -1,7 +1,12 @@
 package com.cs32191617.kwic.components;
 
+import java.util.Arrays;
+
 /**
  * Created by zixian on 8/18/16.
  */
 public class CircularShifter {
+    public String[] generateIndexes(String input){
+        return new String[]{input};
+    }
 }
---
+++ pipesnfilters/test/CircularShifterTest.java
@@ -0,0 +1,24 @@
+/**
+ * Created by zixian on 8/19/16.
+ */
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import com.cs32191617.kwic.components.CircularShifter;
+
+public class CircularShifterTest {
+    @Test
+    public void testShift(){
+        CircularShifter shifter = new CircularShifter();
+        String[] expected = {"a b c d e f", "b c d e f a", "c d e f a b", "d e f a b c", "e f a b c d", "f a b c d e"};
+        List<String> output = Arrays.asList(shifter.generateIndexes("a  b c   d e f"));
+        Assert.assertEquals("Output should have same number of indexes as expected output", output.size(), expected.length);
+        for(String o: expected){
+            Assert.assertTrue("Output should contain all elements of expected output", output.contains(o));
+        }
+    }
+}
