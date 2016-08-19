e68af2f0f4878f4e2780c0c851d04eb754cdefdc
zixian92 <zixian1992@hotmail.com>
2016-08-19 22:11:38 +0800

    Adds user interface to enter input

--- pipesnfilters/src/com/cs32191617/kwic/Main.java
+++ pipesnfilters/src/com/cs32191617/kwic/Main.java
@@ -3,16 +3,39 @@ package com.cs32191617.kwic;
 import com.cs32191617.kwic.components.IgnoreListParser;
 import com.cs32191617.kwic.indexbuilder.IndexBuilder;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
     public static void main(String[] args) {
-        String ignoreInput = "and, the, of";
-        IndexBuilder iBuilder = new IndexBuilder(IgnoreListParser.generateIgnoreList(ignoreInput));
-        iBuilder.generateAndAddIndexes("The lord of the Rings");
-        iBuilder.generateAndAddIndexes("Harry potter and the deathly Hallows");
-        String[] indexes = iBuilder.getIndexes();
-        for(String index: indexes){
-            System.out.println(index);
+        String input;
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+
+            // Read in words to ignore
+            System.out.println("Enter list of words to ignore(comma-separated):");
+            input = reader.readLine();
+            if(input!=null){
+                IndexBuilder iBuilder = new IndexBuilder(IgnoreListParser.generateIgnoreList(input));
+                System.out.println("Enter phrases, 1 on each line. Terminate by entering Ctrl+D");
+
+                // Keep reading inputs and generating indexes
+                input = reader.readLine();
+                while(input!=null){
+                    iBuilder.generateAndAddIndexes(input);
+                    input = reader.readLine();
+                }
+
+                // Print output
+                System.out.println("Keywords:");
+                for(String index: iBuilder.getIndexes()){
+                    System.out.println(index);
+                }
+            }
+        } catch(IOException e){
+            System.out.println("IO error occurred. Terminating application.");
         }
     }
 }
